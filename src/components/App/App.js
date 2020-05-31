ed2ddbd6717871bbd62194ffa42db85d61ef7103
Andrew2432 <drew.devops@gmail.com>
2020-05-31 14:18:53 +0530

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Hello</h1>
+      </Container>
+    );
+  }
+}
+
+export default App;
