973781b32ca527f88da5e36d73c814490377cab2
Andrew2432 <drew.devops@gmail.com>
2020-06-01 16:32:17 +0530

    Fetch data from GitHub

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,31 @@
 import React, { Component, Fragment } from 'react';
 import { Container } from 'react-bootstrap';
+import axios from 'axios';
 import Navigation from '../Layouts/Navigation/Navigation';
 import Users from '../Users/Users';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const usersRes = await axios.get('http://api.github.com/users');
+    this.setState({ users: usersRes.data, loading: false });
+  }
+
   render() {
     return (
       <Fragment>
         <Navigation icon="fab fa-github" title="GitHub Finder" />
         <Container>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </Container>
       </Fragment>
     );
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,50 +3,12 @@ import UserItem from './UserItem';
 import { CardColumns } from 'react-bootstrap';
 
 class Users extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [
-        {
-          id: '1',
-          login: 'defunkt',
-          avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-          html_url: 'https://github.com/defunkt',
-        },
-        {
-          id: '2',
-          login: 'defunkt',
-          avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-          html_url: 'https://github.com/defunkt',
-        },
-        {
-          id: '3',
-          login: 'defunkt',
-          avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-          html_url: 'https://github.com/defunkt',
-        },
-        {
-          id: '4',
-          login: 'defunkt',
-          avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-          html_url: 'https://github.com/defunkt',
-        },
-        {
-          id: '5',
-          login: 'defunkt',
-          avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-          html_url: 'https://github.com/defunkt',
-        },
-      ],
-    };
-  }
-
   generateUser(user) {
     return <UserItem key={user.id} user={user} />;
   }
 
   render() {
-    return <CardColumns>{this.state.users.map(this.generateUser)}</CardColumns>;
+    return <CardColumns>{this.props.users.map(this.generateUser)}</CardColumns>;
   }
 }
 
